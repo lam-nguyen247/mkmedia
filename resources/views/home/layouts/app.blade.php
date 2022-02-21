a0539f02e43a0081f35e71add3f0a98e0d2c9430
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2022-02-21 09:15:38 +0700

    Update app.blade.php

--- resources/views/home/layouts/app.blade.php
+++ resources/views/home/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="google-site-verification" content="XrscSqm4bh48Nw0IZhyH-JO52qXBf45BcSy5xigkL2Y" />
+    <meta name="google-site-verification" content="GxpVzZUSABfW7adX1AkgLcuts3754LLq_Smstart-hg" />
     <meta property="og:image" content="@yield('cover', '/images/home/mkmedia-logo.png')"/>
     <meta property="og:image:alt" content="@yield('cover', '/images/home/mkmedia-logo.png')"/>
     <meta name="twitter:image" content="@yield('cover', '/images/home/mkmedia-logo.png')"/>
