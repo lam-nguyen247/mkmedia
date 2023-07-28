f395b181a5534b42b1cd09af9e5bfd96ac6bf3c0
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2023-07-28 21:25:38 +0700

    Update index.blade.php

--- resources/views/home/channel/index.blade.php
+++ resources/views/home/channel/index.blade.php
@@ -85,8 +85,8 @@
                                 <th>{{$values[0][1]}}</th>
                                 <th>{{$values[0][2]}}</th>
                                 <th>{{$values[0][3]}}</th>
-                                <th>{{$values[0][4]}}</th>
-                                <th>{{$values[0][5]}}</th>
+                                {{-- <th>{{$values[0][4]}}</th>
+                                <th>{{$values[0][5]}}</th> --}}
                                 <th>{{$values[0][6]}}</th>
                                 <th>{{$values[0][7]}}</th>
                                 </tr>
@@ -112,12 +112,12 @@
                                         0 1px 18px 0 rgb(0 0 0 / 12%); padding: 5px 10px; min-width: 61px;" onclick="copy('link_{{$i}}')">Sao chép</button>
                                         </td>
 
-                                    <td>
+                                    {{-- <td>
                                         {{$values[$i][4]}}
                                     </td>
                                     <td>
                                         {{$values[$i][5]}}
-                                    </td>
+                                    </td> --}}
                                     <td>{{number_format($values[$i][6], 0, ',', '.')}} <br/>
                                         <button onclick="go()" style="color:black !important; background-color: #E1CE69; border: 1px solid #E1CE69;  box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%),
                                         0 1px 18px 0 rgb(0 0 0 / 12%); padding: 5px 10px;">Đặt mua</button>
