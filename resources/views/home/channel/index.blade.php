b6c9e0edd262dc6fd7c90b617029e1f9aacb28b3
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2024-05-19 21:44:28 +0700

    Update index.blade.php

--- resources/views/home/channel/index.blade.php
+++ resources/views/home/channel/index.blade.php
@@ -134,11 +134,11 @@
                                         <td>
                                             <input value="{{ $values[$i][3] }}" type="text" style="display: none;"
                                                 id="link_{{ $i }}" />
-                                            <button onclick="xemKenh('{{ $values[$i][3] }}')" title="{{ $values[$i][3] }}"
+                                            <a href="{{ $values[$i][3] }}" title="{{ $values[$i][3] }}"
                                                 alt="{{ $values[$i][3] }}" target="_blank"
                                                 style="color:black !important;  background-color: #2196F3; border: 1px solid #2196F3;  box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%),
-                                        0 1px 18px 0 rgb(0 0 0 / 12%); padding: 5px 10px; margin: 5px; min-width: 61px;"
-                                                onclick="copy('link_{{ $i }}')">Xem group</button>
+                                        0 1px 18px 0 rgb(0 0 0 / 12%); padding: 5px 10px; margin: 5px; min-width: 61px;">Xem
+                                                group</a>
                                             <button
                                                 style="color:black !important; background-color: #8CE78C; border: 1px solid #8CE78C; margin: 5px;  box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%),
                                         0 1px 18px 0 rgb(0 0 0 / 12%); padding: 5px 10px; min-width: 61px;"
