e72012450de738432cf250bf15d22b60362abde9
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2024-05-21 07:49:57 +0700

    Update ChannelController.php

--- app/Http/Controllers/Home/ChannelController.php
+++ app/Http/Controllers/Home/ChannelController.php
@@ -73,25 +73,8 @@ class ChannelController extends Controller
             $accessToken = json_decode(file_get_contents($tokenPath), true);
             $client->setAccessToken($accessToken);
         }
-
         if ($client->isAccessTokenExpired()) {
-            if ($client->getRefreshToken()) {
-                $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-            } else {
-                $authUrl = $client->createAuthUrl();
-                printf("Open the following link in your browser:\n%s\n", $authUrl);
-                print 'Enter verification code: ';
-                $authCode = trim(fgets(STDIN));
-
-                // Exchange authorization code for an access token.
-                $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-                $client->setAccessToken($accessToken);
-
-                // Check to see if there was an error.
-                if (array_key_exists('error', $accessToken)) {
-                    throw new Exception(join(', ', $accessToken));
-                }
-            }
+            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
 
             if (!file_exists(dirname($tokenPath))) {
                 mkdir(dirname($tokenPath), 0700, true);
